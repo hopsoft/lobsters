b5e7bde7b976aeaf16a45a9db66c7b7ce1a5b38f
Nathan Hopkins <natehop@gmail.com>
2012-09-04 11:34:35 -0600

    Added a secret token.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lobsters::Application.initialize!
+Lobsters::Application.config.secret_token = ENV["SECRET_TOKEN"] || 'e8fd2f339970dbc4767c8453aff3a39410983aef1b46d73f9df873562a5300ad'
